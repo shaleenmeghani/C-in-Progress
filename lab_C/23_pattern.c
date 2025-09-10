a3fbab08dba83313d8875102b277ec0a63bf5072
Shaleen Meghani <101651589+shaleenmeghani@users.noreply.github.com>
2025-09-10 20:09:33 +0530

    right angled triangle

--- lab_C/23_pattern.c
+++ lab_C/23_pattern.c
@@ -7,7 +7,7 @@ int main(){
 
     for(int i=1;i<=number;i++)//this loop decides the rows
     {
-        for(int j=0;j<i;j++)//this loop decides amount in row
+        for(int j=1;j<=i;j++)//this loop decides amount in row
         {
             printf("*");
         }
