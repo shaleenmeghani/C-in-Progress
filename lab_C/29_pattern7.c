8c377ee831a686267027c2e1086bfc56fed561e6
Shaleen Meghani <101651589+shaleenmeghani@users.noreply.github.com>
2025-10-02 13:12:34 +0530

    Add files via upload

---
+++ lab_C/29_pattern7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    int row;
+    printf("enter rows:");
+    scanf("%d",&row);
+
+    for(int upperi=1;upperi<=row;upperi++)
+    {
+        for(int uppers=1;uppers<=row-upperi;uppers++)
+        {
+            printf(" ");
+        }
+        for(int upperj=1;upperj<=upperi*2-1;upperj++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int loweri=row;loweri>0;loweri--)
+    {
+        for(int lowers=1;lowers<=row-loweri;lowers++)
+        {
+            printf(" ");
+        }
+        for(int lowerj=1;lowerj<=loweri*2-1;lowerj++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
